257ce48c36e8d92865f08844fcaf38278f21ae69
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-02 14:55:13 -0500

    Use remote docker

--- src/scripts/install_aws_cli.sh
+++ src/scripts/install_aws_cli.sh
@@ -1,6 +1,6 @@
-yum update -y
-yum install -y curl unzip
-curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64${AWS_CLI_VER_STRING}.zip" -o "awscliv2.zip"
+apt-get update -y
+apt-get install -y curl unzip
+curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q -o awscliv2.zip
 ./aws/install
 rm awscliv2.zip
