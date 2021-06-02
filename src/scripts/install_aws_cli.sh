be2f9f5e28b1e74ba2ed8561f52048b60e5bce38
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-02 12:16:03 -0500

    Remove sudo

--- src/scripts/install_aws_cli.sh
+++ src/scripts/install_aws_cli.sh
@@ -1,6 +1,6 @@
-sudo apt-get update -y
-sudo apt-get install -y curl
+apt-get update -y
+apt-get install -y curl
 curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64${AWS_CLI_VER_STRING}.zip" -o "awscliv2.zip"
 unzip -q -o awscliv2.zip
-sudo ./aws/install
+./aws/install
 rm awscliv2.zip
