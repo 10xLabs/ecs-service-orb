abcc80a418a67dac83d7924a9cef581db3a3e103
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-02 13:03:18 -0500

    Change to centos

--- src/scripts/install_docker.sh
+++ src/scripts/install_docker.sh
@@ -1,15 +1,9 @@
-apt-get update -y
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io
-systemctl status docker
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --enable docker-ce-nightly
+sudo yum-config-manager --enable docker-ce-test
+sudo yum-config-manager --disable docker-ce-nightly
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
