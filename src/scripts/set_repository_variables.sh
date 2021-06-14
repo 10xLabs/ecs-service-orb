b82dc211ce1a9602848797346b19d017d84ed20f
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-14 11:50:28 -0500

    cahnge stack putput

--- src/scripts/set_repository_variables.sh
+++ src/scripts/set_repository_variables.sh
@@ -1,2 +1,2 @@
-echo "export REPOSITORY_ENDPOINT=$(pulumi stack output RepositoryEndpoint --cwd pulumi --stack "$STACK_NAME")" >> "$BASH_ENV"
-echo "export REPOSITORY_IMAGE_NAME=$(pulumi stack output RepositoryImageName --cwd pulumi --stack "$STACK_NAME")" >> "$BASH_ENV"
+echo "export REPOSITORY_ENDPOINT=$(pulumi stack output stack --json --cwd pulumi --stack "$STACK_NAME" | jq '.repository.endpoint')" >> "$BASH_ENV"
+echo "export REPOSITORY_IMAGE_NAME=$(pulumi stack output stack --json --cwd pulumi --stack "$STACK_NAME" | jq '.repository.imageName')" >> "$BASH_ENV"
