322b7a84cee3a385338d1c9ef2dccc6b370327b6
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-20 10:32:16 -0500

    feat: add release configuration

--- src/scripts/set_repository_variables.sh
+++ src/scripts/set_repository_variables.sh
@@ -1,2 +1,2 @@
-echo "export REPOSITORY_ENDPOINT=$(pulumi stack output stack --json --cwd deploy --stack "$STACK_NAME" | jq '.repository.endpoint')" >> "$BASH_ENV"
-echo "export REPOSITORY_IMAGE_NAME=$(pulumi stack output stack --json --cwd deploy --stack "$STACK_NAME" | jq '.repository.imageName')" >> "$BASH_ENV"
+echo "export REPOSITORY_ENDPOINT=$(pulumi stack output stack --json --cwd "$WORKING_DIRECTORY" --stack "$STACK_NAME" | jq '.repository.endpoint')" >> "$BASH_ENV"
+echo "export REPOSITORY_IMAGE_NAME=$(pulumi stack output stack --json --cwd "$WORKING_DIRECTORY" --stack "$STACK_NAME" | jq '.repository.imageName')" >> "$BASH_ENV"
