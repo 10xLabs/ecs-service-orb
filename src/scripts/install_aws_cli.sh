79d4fe8c6115d73a672e25fa71235a0fa509fd53
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-20 11:29:49 -0500

    fix: change node image

--- src/scripts/install_aws_cli.sh
+++ src/scripts/install_aws_cli.sh
@@ -1,5 +1,5 @@
-sudo apt-get update -y > /dev/null
-sudo apt-get install -y curl unzip > /dev/null
+# sudo apt-get update -y > /dev/null
+# sudo apt-get install -y curl unzip > /dev/null
 curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q -o awscliv2.zip
 sudo ./aws/install
